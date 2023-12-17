5261d23476d7bb836abbab491c24d72f3af8ed83
rehman-talha <93119114+rehman-talha@users.noreply.github.com>
2023-12-17 20:22:17 +0500

    Update popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -1,45 +1,5 @@
-// popup/popup.js
+// popup.js
 
-document.addEventListener('DOMContentLoaded', function () {
-  // Add event listener to handle export button click
-  document.getElementById('exportButton').addEventListener('click', handleExport);
-
-  // Add event listener to handle import button click
-  document.getElementById('importButton').addEventListener('change', handleImport);
-
-  // Add event listener to handle view words button click
-  document.getElementById('viewWordsButton').addEventListener('click', handleViewWords);
-});
-
-// Function to handle export
-function handleExport() {
-  // Send a message to the background script to get all words
-  browser.runtime.sendMessage({ action: 'getAllWords' })
-    .then(response => {
-      const wordwiseWords = response.words;
-
-      // Convert wordwiseWords to CSV format
-      const csvContent = Object.entries(wordwiseWords)
-        .map(([word, definition]) => `${word},${definition}`)
-        .join('\n');
-
-      // Create a Blob with the CSV content
-      const blob = new Blob([csvContent], { type: 'text/csv' });
-
-      // Create a download link and trigger the download
-      const link = document.createElement('a');
-      link.href = URL.createObjectURL(blob);
-      link.download = 'wordwise_export.csv';
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    })
-    .catch(error => {
-      console.error('Error exporting words:', error);
-    });
-}
-
-// Function to handle import
 function handleImport(event) {
   const fileInput = event.target;
   const file = fileInput.files[0];
@@ -71,12 +31,45 @@ function handleImport(event) {
   }
 }
 
-// Function to handle view words
+function handleExport() {
+  // Send a message to the background script to get all words
+  browser.runtime.sendMessage({ action: 'getAllWords' })
+    .then(response => {
+      const wordwiseWords = response.words;
+
+      // Convert wordwiseWords to CSV format
+      const csvContent = Object.entries(wordwiseWords)
+        .map(([word, definition]) => `${word},${definition}`)
+        .join('\n');
+
+      // Create a Blob with the CSV content
+      const blob = new Blob([csvContent], { type: 'text/csv' });
+
+      // Create a download link and trigger the download
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      link.download = 'wordwise_export.csv';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    })
+    .catch(error => {
+      console.error('Error exporting words:', error);
+    });
+}
+
 function handleViewWords() {
-  // Send a message to the background script to view words
-  browser.runtime.sendMessage({ action: 'viewWords' }, function (response) {
-    const words = response.words || {};
-    const wordsList = Object.entries(words).map(([word, definition]) => `${word}: ${definition}`);
-    alert(wordsList.join('\n'));
-  });
+  // Send a message to the background script to view saved words
+  browser.runtime.sendMessage({ action: 'viewWords' })
+    .then(response => {
+      const wordwiseWords = response.words;
+      console.log('Viewing Words:', wordwiseWords);
+
+      // You can add more logic to display the words
+      // For example, open a new tab or a popup to display the words
+      // This is just a placeholder for the actual logic
+    })
+    .catch(error => {
+      console.error('Error viewing words:', error);
+    });
 }
