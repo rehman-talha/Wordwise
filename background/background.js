186178d6ded55283a8fc14f6d5ca76132e9e3a49
rehman-talha <93119114+rehman-talha@users.noreply.github.com>
2023-12-17 12:51:07 +0500

    Create background.js

---
+++ background/background.js
@@ -0,0 +1,18 @@
+// background/background.js
+
+// Example: Log a message when the extension is installed
+browser.runtime.onInstalled.addListener(function(details) {
+  if (details.reason === 'install') {
+    console.log('Extension installed!');
+  } else if (details.reason === 'update') {
+    console.log('Extension updated!');
+  }
+});
+
+// Example: Listen for a message from the content script
+browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  console.log('Message received in background:', request);
+  
+  // Perform background tasks or send a response if needed
+  sendResponse({ message: 'Background script received your message!' });
+});
