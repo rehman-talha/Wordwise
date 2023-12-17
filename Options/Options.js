e79b351b172ef892da509752c3dea467aba18619
rehman-talha <93119114+rehman-talha@users.noreply.github.com>
2023-12-17 18:23:58 +0500

    Update Options.js

--- Options/Options.js
+++ Options/Options.js
@@ -1,36 +1,40 @@
 // options/options.js
 document.addEventListener('DOMContentLoaded', function() {
-  // Load saved options on page load
-  loadOptions();
+  // Add event listener to handle double-click and show the modal window
+  document.addEventListener('dblclick', showDoubleClickModal);
 
-  // Add event listener to save options when the "Save Options" button is clicked
-  document.getElementById('saveOptions').addEventListener('click', saveOptions);
+  // Add event listener to save meaning when the "Save Meaning" button is clicked
+  document.getElementById('saveMeaning').addEventListener('click', saveMeaning);
+
+  // Add event listener to close the modal when the "Close" button is clicked
+  document.getElementById('closeModal').addEventListener('click', hideDoubleClickModal);
 });
 
-// Function to load saved options
-function loadOptions() {
-  browser.storage.sync.get(['difficultyLevel', 'apiKey'], function(result) {
-    // Set the selected difficulty level in the dropdown
-    const difficultyLevel = result.difficultyLevel || '1';
-    document.getElementById('difficultyLevel').value = difficultyLevel;
-
-    // Set the saved API key in the input field
-    const apiKey = result.apiKey || '';
-    document.getElementById('apiKey').value = apiKey;
-  });
+// Function to show the double-click modal window
+function showDoubleClickModal(event) {
+  const selectedWord = window.getSelection().toString().trim();
+  if (selectedWord) {
+    document.getElementById('word').value = selectedWord;
+    document.getElementById('meaning').value = ''; // Clear previous meaning
+    document.getElementById('doubleClickModal').style.display = 'block';
+  }
 }
 
-// Function to save options
-function saveOptions() {
-  // Get the selected difficulty level
-  const difficultyLevel = document.getElementById('difficultyLevel').value;
+// Function to save meaning
+function saveMeaning() {
+  const word = document.getElementById('word').value;
+  const meaning = document.getElementById('meaning').value;
 
-  // Get the entered API key
-  const apiKey = document.getElementById('apiKey').value.trim();
+  // Save the word and its meaning (implement your storage logic here)
 
-  // Save the difficulty level and API key in Firefox storage
-  browser.storage.sync.set({ difficultyLevel, apiKey }, function() {
-    // Notify the user that options were saved
-    alert('Options saved!');
-  });
+  // Notify the user that the meaning was saved
+  alert(`Meaning for '${word}' saved: ${meaning}`);
+
+  // Close the modal window
+  hideDoubleClickModal();
+}
+
+// Function to hide the double-click modal window
+function hideDoubleClickModal() {
+  document.getElementById('doubleClickModal').style.display = 'none';
 }
