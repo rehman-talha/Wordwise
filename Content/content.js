8b003fc99b3792839d2e593e9b541c49ab60b3ef
rehman-talha <93119114+rehman-talha@users.noreply.github.com>
2023-12-17 12:56:30 +0500

    Update content.js

--- Content/content.js
+++ Content/content.js
@@ -3,12 +3,49 @@
 // Example: Log a message when the content script is executed
 console.log('Content script is running!');
 
-// Example: Send a message to the background script
-chrome.runtime.sendMessage({ message: 'Hello from content script!' }, function(response) {
-  console.log('Response from background script:', response.message);
-});
+// Function to load definitions for difficult words
+function loadDefinitions() {
+  // Example: Get all paragraphs on the page
+  const paragraphs = document.querySelectorAll('p');
 
-// Example: Manipulate the DOM (replace all paragraphs with a message)
-document.querySelectorAll('p').forEach(function(paragraph) {
-  paragraph.textContent = 'This paragraph has been replaced by the content script!';
-});
+  // Example: Loop through paragraphs and add definitions
+  paragraphs.forEach(function (paragraph) {
+    const words = paragraph.textContent.split(' ');
+
+    // Example: Check each word for difficulty and add definitions
+    words.forEach(function (word) {
+      // Replace this with your logic to determine if a word is difficult
+      const isDifficult = isWordDifficult(word);
+
+      if (isDifficult) {
+        // Replace this with your logic to fetch the definition
+        const definition = getWordDefinition(word);
+
+        // Add the definition as a tooltip or small text near the word
+        const span = document.createElement('span');
+        span.textContent = ` (${definition}) `;
+        span.style.fontSize = 'small'; // Adjust the font size as needed
+
+        // Wrap the word with the span
+        paragraph.innerHTML = paragraph.innerHTML.replace(word, span.outerHTML);
+      }
+    });
+  });
+}
+
+// Function to determine if a word is difficult (replace this with your logic)
+function isWordDifficult(word) {
+  // Replace this with your logic to determine word difficulty
+  // For now, consider every word as difficult
+  return true;
+}
+
+// Function to fetch the definition of a word (replace this with your logic)
+function getWordDefinition(word) {
+  // Replace this with your logic to fetch the definition
+  // For now, return a placeholder definition
+  return 'Definition of ' + word;
+}
+
+// Load definitions when the content script is executed
+loadDefinitions();
