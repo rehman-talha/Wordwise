35245fc4209de83b72a00442b4da0092d29ccd13
rehman-talha <93119114+rehman-talha@users.noreply.github.com>
2023-12-17 13:30:00 +0500

    Update content.js

--- Content/content.js
+++ Content/content.js
@@ -9,11 +9,35 @@ function getUserDifficultyLevel() {
 
 // Function to fetch definitions using ChatGPT API
 async function fetchDefinitionFromChatGPT(word) {
-  // You need to implement logic to fetch definitions using ChatGPT API
-  // For now, let's assume a placeholder implementation
-  const response = await fetch(`https://api.example.com/chatgpt?word=${word}`);
-  const data = await response.json();
-  return data.definition;
+  // Replace 'YOUR_CHATGPT_API_KEY' with your actual ChatGPT API key
+  const apiKey = 'YOUR_CHATGPT_API_KEY';
+  const endpoint = 'https://api.openai.com/v1/chat/completions'; // Adjust the endpoint based on OpenAI's API documentation
+
+  // You may need to format the input according to the ChatGPT API requirements
+  const input = `Define the word "${word}"`;
+
+  try {
+    const response = await fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`
+      },
+      body: JSON.stringify({
+        messages: [
+          { role: 'system', content: 'You are a helpful assistant.' },
+          { role: 'user', content: input }
+        ]
+      })
+    });
+
+    const data = await response.json();
+    // Extract and return the definition from the response
+    return data.choices[0]?.message?.content || 'Definition not found';
+  } catch (error) {
+    console.error('Error fetching definition:', error.message);
+    return 'Failed to fetch definition';
+  }
 }
 
 // Function to load difficult words based on user's selected difficulty level
