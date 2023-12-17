1ffde2248b86e25e2d73cb3a971d6cdd1b820c37
rehman-talha <93119114+rehman-talha@users.noreply.github.com>
2023-12-17 13:34:32 +0500

    Update content.js

--- Content/content.js
+++ Content/content.js
@@ -1,67 +1,17 @@
 // content/content.js
 
-// Function to fetch the user's selected difficulty level from extension options
-function getUserDifficultyLevel() {
-  // You need to implement logic to fetch the user's preferred difficulty level from extension options
-  // For now, let's assume a default value of 1
-  return 1;
-}
-
-// Function to fetch definitions using ChatGPT API
-async function fetchDefinitionFromChatGPT(word) {
-  // Replace 'YOUR_CHATGPT_API_KEY' with your actual ChatGPT API key
-  const apiKey = 'YOUR_CHATGPT_API_KEY';
-  const endpoint = 'https://api.openai.com/v1/chat/completions'; // Adjust the endpoint based on OpenAI's API documentation
-
-  // You may need to format the input according to the ChatGPT API requirements
-  const input = `Define the word "${word}"`;
-
-  try {
-    const response = await fetch(endpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`
-      },
-      body: JSON.stringify({
-        messages: [
-          { role: 'system', content: 'You are a helpful assistant.' },
-          { role: 'user', content: input }
-        ]
-      })
+// Function to fetch the ChatGPT API key from extension options
+async function getChatGPTApiKey() {
+  return new Promise(resolve => {
+    // Retrieve the API key from chrome.storage.sync
+    chrome.storage.sync.get(['apiKey'], function(result) {
+      const apiKey = result.apiKey || '';
+      resolve(apiKey);
     });
-
-    const data = await response.json();
-    // Extract and return the definition from the response
-    return data.choices[0]?.message?.content || 'Definition not found';
-  } catch (error) {
-    console.error('Error fetching definition:', error.message);
-    return 'Failed to fetch definition';
-  }
-}
-
-// Function to load difficult words based on user's selected difficulty level
-function loadDifficultWords() {
-  const userDifficultyLevel = getUserDifficultyLevel();
-
-  // Fetch the difficult words based on the user's difficulty level
-  const difficultWords = getAllDifficultWords(userDifficultyLevel);
-
-  // Process difficult words and fetch definitions
-  difficultWords.forEach(async word => {
-    const definition = await fetchDefinitionFromChatGPT(word);
-    // Display or store the definition as needed
-    console.log(`${word}: ${definition}`);
   });
 }
 
-// Function to get all difficult words based on user's difficulty level
-function getAllDifficultWords(userDifficultyLevel) {
-  // You need to implement logic to fetch words based on user's difficulty level
-  // For now, let's assume a placeholder implementation
-  const allWords = ['word1', 'word2', 'word3', 'word4', 'word5'];
-  return allWords.slice(userDifficultyLevel - 1);
-}
+// ... (rest of the content.js code remains unchanged)
 
 // Load difficult words when the content script is executed
 loadDifficultWords();
